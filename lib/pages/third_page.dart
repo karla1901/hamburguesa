9de1daf941e4727dd8b8646f5aa9e16fa84ba86b
FlutLab.io <>
2021-05-26 17:06:16 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -1,15 +1,121 @@
 import 'package:flutter/material.dart';
 
-class ThirdPage extends StatelessWidget {
+class ThirdPage extends StatefulWidget {
+  @override
+  _ThirdPageState createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Tercera Página',
-          style: new TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
-        ), //Text
-      ), // center
-    ); //container
-  } // widget
-} // Clase ThirdPage
+    return Material(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 300, minWidth: 200),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 80.0,
+              ),
+              Container(
+                color: Colors.amber,
+                height: 200,
+                width: 350,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 200,
+                width: 250,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.blue,
+                      Colors.blue[300],
+                      Colors.blue[200],
+                    ],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
+                  ),
+                ),
+                child: Container(
+                  padding: EdgeInsets.only(top: 35.0, bottom: 16),
+                  child: Text(
+                    'Karliux',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 50,
+                width: 100,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.purple,
+                      Colors.purple[300],
+                      Colors.purple[200],
+                    ],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
+                  ),
+                ),
+                child: Text(
+                  'yeehaw',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              InkWell(
+                onTap: () {
+                  print('sky');
+                },
+                child: Container(
+                  width: 110,
+                  height: 110,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.green,
+                        Colors.green[300],
+                        Colors.green[200],
+                      ],
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                    ),
+                  ),
+                  child: ImageIcon(
+                    AssetImage("assets/images/logo.png"),
+                    size: 100,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
