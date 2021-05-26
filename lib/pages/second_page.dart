4d8684e1bce9a3c8d8f6d9bb1a625c336008dbc5
FlutLab.io <>
2021-05-26 16:57:32 +0000

    Autosave

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -5,46 +5,116 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ui.Size logicalSize = MediaQuery.of(context).size;
-    final double height = logicalSize.height;
+    final double _height = logicalSize.height;
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Container(
           width: double.infinity,
           height: double.infinity,
-          //color:Colors.green,
-          padding: EdgeInsets.symmetric(horizontal:30.0),
-          child: Column(
-          children: <Widget>[
-            SizedBox(height:100,),
-            Container(
-            child: Text('WENDY´S',
-            style: new TextStyle(fontWeight: FontWeight.bold,
-            fontSize: 45.0, color: Colors.redAccent),),
-            ),//container
-            SizedBox(height:40,),
-            Column(
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          child: SingleChildScrollView(
+            child: Column(
               children: <Widget>[
-                Container( 
-                  padding: EdgeInsets.all(2.0),
-                  child: Stack(
+                SizedBox(height: 65),
+                Container(
+                  child: Column(
                     children: <Widget>[
-                      Align(
-                        child: new Image(
-                          width:300.0,
-                          height:200.0,
-                          image: new AssetsImage('assets/images/burger1.png'),
-                        )//image
-                      )//align
-                    ],//<widget>[]
-                  ),//stack
-                )//container
-              ],//<widget>[]
-            ),//column
-            ],//<widget>[]
-          ),//column
-        )//container
-      ), //safeArea
-    ); //scaffold
-  } // widget
-} // Clase SecondPage
+                      Text(
+                        'WENDY´S',
+                        style: new TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40.0,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Text(
+                        'Karla Castañeda',
+                        style: new TextStyle(
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 3.0,
+                          fontSize: 12.0,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ), //contenedor
+                SizedBox(height: 40),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(2.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Align(
+                            child: new Image(
+                              width: 250.0,
+                              height: 165.1,
+                              image: new AssetImage('assets/images/burger1.png'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.amber.shade100,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide.none,
+                        ),
+                        suffixIcon: Icon(Icons.account_box),
+                        hintText: 'Nombre de usuario',
+                      ),
+                    ), //Campo de Texto
+                    SizedBox(height: 16),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.amber.shade100,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide.none,
+                        ),
+                        suffixIcon: Icon(Icons.vpn_key_rounded),
+                        hintText: 'Contraseña',
+                      ),
+                    ), //Campo de Texto
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.all(20.0),
+                              primary: Colors.orange,
+                            ),
+                            child: Text('Iniciar Sesión'),
+                            onPressed: () {}), // Botón Login
+                        SizedBox(
+                          width: 10,
+                        ),
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.all(20.0),
+                              primary: Colors.red,
+                            ),
+                            child: Text('Cancelar'),
+                            onPressed: () {}), // Botón Login
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
