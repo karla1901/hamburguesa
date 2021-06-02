4e10c07f0da743663ba2194e5fa30aa93c9865a1
FlutLab.io <>
2021-06-02 03:58:07 +0000

    Autosave

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -8,9 +8,6 @@ class FirstPage extends StatelessWidget {
         child: Container(
           child: Column(
             children: <Widget>[
-              // Image(
-              //   image: NetworkImage('https://hipertextual.com/files/2019/09/hipertextual-the-legend-of-zelda-links-awakening-2019999870.jpg'),
-              // ),
               FadeInImage(
                 placeholder: AssetImage('assets/loading.gif'),
                 image: NetworkImage('https://raw.githubusercontent.com/karla1901/MisFotos/main/Sam.jpg'),
