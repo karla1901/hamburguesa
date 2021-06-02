bf5d601361cd5165ead1c5fbc9f7cbbd23d0cd06
FlutLab.io <>
2021-06-02 04:49:12 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -62,7 +62,7 @@ class _ThirdPageState extends State<ThirdPage> {
               borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
               child: FadeInImage(
                 placeholder: AssetImage('assets/loading.gif'),
-                image: NetworkImage('https://raw.githubusercontent.com/karla1901/MisFotos/main/vuelo.jpg'),
+                image: NetworkImage('https://raw.githubusercontent.com/karla1901/MisFotos/main/aero.jpg'),
               ),
             ),
             Padding(
