293b437b82f98dcc8acd49eb0b84d3f94817b87d
FlutLab.io <>
2021-06-02 04:33:26 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -6,113 +6,94 @@ class ThirdPage extends StatefulWidget {
 }
 
 class _ThirdPageState extends State<ThirdPage> {
-  @override
+    @override
   Widget build(BuildContext context) {
-    return Material(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(maxWidth: 50, minWidth: 20),
-        child: SingleChildScrollView(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards de Karla'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Center(
           child: Column(
             children: <Widget>[
-              SizedBox(
-                height: 80.0,
-              ),
-              Container(
-                color: Colors.lime,
-                height: 200,
-                width: 350,
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 200,
-                width: 250,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.orangeAccent,
-                      Colors.orangeAccent[100],
-                    ],
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                  ),
-                ),
-                child: Container(
-                  padding: EdgeInsets.only(top: 35.0, bottom: 16),
-                  child: Text(
-                    'Karla Castañeda 6J',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 50,
-                width: 100,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  gradient: LinearGradient(
-                    colors: [
-                       Colors.deepPurpleAccent[700],
-                       Colors.deepPurpleAccent,
-                      Colors.deepPurpleAccent[100],
-                    ],
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                  ),
-                ),
-                child: Text(
-                  'Hola',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              InkWell(
-                onTap: () {
-                  print('nube');
-                },
-                child: Container(
-                  width: 110,
-                  height: 110,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30.0),
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.purple[900],
-                        Colors.purple[700],
-                        Colors.purple,
-                      ],
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                    ),
-                  ),
-                  child: ImageIcon(
-                    AssetImage("assets/images/nu.png"),
-                    size: 100,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+              _crearCard1(),
+              _crearCard2(),
+              _crearCard3()
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearCard1() {
+    return Card(
+      elevation: 5,
+      color: Color(0xFFA1887F),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              '¿Qué es Flutter?',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Flutter se usa principalmente para desarrollar aplicaciones de Android y iOS sin necesidad de escribir un código base propio para cada uno de estos sistemas, completamente diferentes entre sí. En este contexto, las aplicaciones móviles se ejecutan como auténticas aplicaciones nativas en los dispositivos.')
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearCard2() {
+    return Card(
+      elevation: 5,
+      color: Color(0xFFBDBDBD),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              '¿Qué es mejor, Android o iOS?',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Sabemos que Android toma la delantera en cuanto a variedad de costos -ofreciendo smartphones a precios accesibles-, batería y sistema de navegación. Mientras que los equipos Iphone ofrecen mayor calidad de plataforma gracias al sistema operativo iOS que se caracteriza por ofrecer un rendimiento de gama alta.',
+              style: TextStyle(color: Colors.black),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearCard3() {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/loading.gif'),
+                image: NetworkImage('https://raw.githubusercontent.com/karla1901/MisFotos/main/iosAndroid.png'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Card con imágen'),
+            )
+          ],
         ),
       ),
     );
