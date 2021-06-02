041a76c6c883c20e95a77505398ac34b6b131f76
FlutLab.io <>
2021-06-02 05:19:09 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -98,6 +98,7 @@ class _ThirdPageState extends State<ThirdPage> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text('Huatulco'),
+              color: Colors.white,
             )
           ],
         ),
