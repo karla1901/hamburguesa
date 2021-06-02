ffeb8558013ca8a7cd0ad435aeedb632d2a53cc0
FlutLab.io <>
2021-06-02 03:57:38 +0000

    Autosave

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -3,13 +3,22 @@ import 'package:flutter/material.dart';
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Primera Página',
-          style: new TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
-        ), //Text
-      ), // center
-    ); //container
-  } // widget
-} // Clase FirstPage
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              // Image(
+              //   image: NetworkImage('https://hipertextual.com/files/2019/09/hipertextual-the-legend-of-zelda-links-awakening-2019999870.jpg'),
+              // ),
+              FadeInImage(
+                placeholder: AssetImage('assets/loading.gif'),
+                image: NetworkImage('https://raw.githubusercontent.com/karla1901/MisFotos/main/Sam.jpg'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
