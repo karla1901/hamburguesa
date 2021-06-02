8ece802392a3e546b192dddaae5d5e86104556af
FlutLab.io <>
2021-06-02 04:51:04 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -24,31 +24,27 @@ class _ThirdPageState extends State<ThirdPage> {
     );
   }
 
-  Widget _crearCard1() {
+   Widget _crearCard1() {
     return Card(
       elevation: 5,
       color: Color(0xFFA1887F),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: Container(
+        padding: EdgeInsets.all(20),
         child: Column(
           children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
-              child: FadeInImage(
-                placeholder: AssetImage('assets/loading.gif'),
-                image: NetworkImage('https://raw.githubusercontent.com/karla1901/MisFotos/main/aero.jpg'),
-              ),
+            Text(
+              '¿Qué es Flutter?',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('Card con imágen'),
-            )
+            SizedBox(
+              height: 20,
+            ),
+            Text('Flutter se usa principalmente para desarrollar aplicaciones de Android y iOS sin necesidad de escribir un código base propio para cada uno de estos sistemas, completamente diferentes entre sí. En este contexto, las aplicaciones móviles se ejecutan como auténticas aplicaciones nativas en los dispositivos.')
           ],
         ),
       ),
     );
   }
-}
 
   Widget _crearCard2() {
     return Card(
@@ -56,30 +52,29 @@ class _ThirdPageState extends State<ThirdPage> {
       color: Color(0xFFBDBDBD),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: Container(
+        padding: EdgeInsets.all(20),
         child: Column(
           children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
-              child: FadeInImage(
-                placeholder: AssetImage('assets/loading.gif'),
-                image: NetworkImage('https://raw.githubusercontent.com/karla1901/MisFotos/main/aero.jpg'),
-              ),
+            Text(
+              '¿Qué es mejor, Android o iOS?',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('Card con imágen'),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Sabemos que Android toma la delantera en cuanto a variedad de costos -ofreciendo smartphones a precios accesibles-, batería y sistema de navegación. Mientras que los equipos Iphone ofrecen mayor calidad de plataforma gracias al sistema operativo iOS que se caracteriza por ofrecer un rendimiento de gama alta.',
+              style: TextStyle(color: Colors.black),
             )
           ],
         ),
       ),
     );
   }
-}
 
   Widget _crearCard3() {
     return Card(
       elevation: 5,
-      color: Color(0xFFA1887F),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: Container(
         child: Column(
