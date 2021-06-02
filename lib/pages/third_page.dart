194981ac3dd832f92131f81ad84f940ac13a68e7
FlutLab.io <>
2021-06-02 04:41:07 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class ThirdPage extends StatefulWidget {
   @override
-  _ThirdPageState createState() => _ThirdPageState();
    theme: ThemeData(primarySwatch: Colors.brown),
+  _ThirdPageState createState() => _ThirdPageState();
 }
 
 class _ThirdPageState extends State<ThirdPage> {
